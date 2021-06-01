ed8aa7d2cc7a7a44b497331d5ee871e406060ed7
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-06-01 15:41:46 -0500

    completed self test

--- solutions.js
+++ solutions.js
@@ -20,3 +20,46 @@
  * and
  * concat
  */
+
+function isTrue(input) {
+    return input === true;
+}
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input){
+    return !(input);
+}
+
+function addOne(number){
+    return Number(number) + 1;
+}
+
+function isEven(input){
+    return input % 2 === 0;
+}
+
+function isIdentical(x,y){
+    return x === y;
+}
+
+function isEqual(x,y){
+    return x == y;
+}
+
+function or(x,y){
+    return x || y;
+}
+
+function and(x,y){
+    return x && y;
+}
+
+
+
+function concat(x, y){
+    let string = ''
+    return string.concat(x,y);
+}
